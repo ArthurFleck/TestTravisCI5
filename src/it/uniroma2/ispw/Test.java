1fec2afe26cccf4dcfae8baaba1a35b95df035ec
ArthurFleck <56659604+ArthurFleck@users.noreply.github.com>
2019-10-27 16:54:32 +0100

    final

---
+++ src/it/uniroma2/ispw/Test.java
@@ -0,0 +1,12 @@
+package it.uniroma2.ispw;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!!!");
+		
+	}
+
+}
